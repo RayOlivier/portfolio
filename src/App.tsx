b6a86d014de852682c40980d066485b7de800c49
Ray Olivier <rayolivier@outlook.com>
2023-12-11 13:27:02 -0600

    scss set up and reset

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ function App() {
   return (
     <>
       <div>header</div>
-      <div>hi, i'm ray - about me stuff</div>
+      <div>
+        <div className="eyebrow">Hi</div>
+        <h1>I'm Ray and I make websites.</h1>
+        <h2>some tagline BS</h2>
+        hi, i'm ray - about me stuff
+      </div>
       <div>skills</div>
       <div>experience</div>
       <div>portfolio - cards? link to github</div>

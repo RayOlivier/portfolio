7793bfa20ece2a45f0e3936ee171f58f387c9a67
Ray Olivier <rayolivier@outlook.com>
2023-12-11 12:15:32 -0600

    basic app set up with vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div>header</div>
+      <div>hi, i'm ray - about me stuff</div>
+      <div>skills</div>
+      <div>experience</div>
+      <div>portfolio - cards? link to github</div>
+      <div>footer</div>
+    </>
+  );
+}
+
+export default App;

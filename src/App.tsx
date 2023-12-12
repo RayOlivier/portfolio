8fa57c955b5380d0b504a8b4fa21446846ca22b8
Ray Olivier <rayolivier@outlook.com>
2023-12-11 18:02:44 -0600

    fill out basic app structure and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import './App.scss';
+import Footer from './components/footer/footer';
+import Nav from './components/nav/nav';
 
 function App() {
   return (
     <>
-      <div>header</div>
-      <div>
-        <div className="eyebrow">Hi</div>
-        <h1>I'm Ray and I make websites.</h1>
-        <h2>some tagline BS</h2>
-        hi, i'm ray - about me stuff
-      </div>
-      <div>skills</div>
-      <div>experience</div>
-      <div>portfolio - cards? link to github</div>
-      <div>footer</div>
+      <Nav></Nav>
+      <main className="main">
+        <section className="section landing">
+          <div className="eyebrow">Hi</div>
+          <h1 className="introduction">
+            I'm <span>Ray</span> and I make websites.
+          </h1>
+          <p className="tagline">some tagline BS</p>
+        </section>
+        <section className="section">
+          <h2>Skills</h2>
+          <p>skills go here</p>
+        </section>
+        <section className="section">
+          <h2>Experience</h2>
+          <p>experience list go here, link to resume??</p>
+        </section>
+        <section className="section">
+          <h2>Portfolio</h2>
+          <p>portfolio cards go here, link to github</p>
+        </section>
+        <section className="section">
+          <h2>Contact</h2>
+          <p>links to email, linkedin, github</p>
+        </section>
+      </main>
+      <Footer> </Footer>
     </>
   );
 }

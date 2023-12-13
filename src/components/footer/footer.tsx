b40117c98161c7b63381d675414adc9e80c61b13
Ray Olivier <rayolivier@outlook.com>
2023-12-13 15:59:35 -0600

    adding react-icons and adding social links to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './footer.scss';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md';
 
 type FooterProps = {
   children?: React.ReactNode;
@@ -8,8 +10,24 @@ type FooterProps = {
 const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className="footer">
-      <div className="social-links"></div>
-      Designed and built by Ray Olivier
+      <ul className="social-links">
+        <li>
+          <a href="mailto:rayolivier@outlook.com">
+            <MdEmail size="3rem" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/rayolivier/" target="_blank">
+            <AiFillLinkedin size="3rem" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/RayOlivier">
+            <AiFillGithub size="3rem" />
+          </a>
+        </li>
+      </ul>
+      Designed and built by Ray Olivier.
     </footer>
   );
 };
